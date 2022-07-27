79f3e20fa880b91b1c1fd8555cc933485a5306ea
Quân Phạm <h311kj113r@gmail.com>
2022-07-27 07:57:12 +0700

    debug

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask, session
+from flask import Flask
 from flask_session import Session
-from src import routes
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+from src import routes
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
-from src import app, session
-from flask import render_template, redirect, url_for, request
+from src import app
+from flask import render_template, redirect, url_for, session, request
+from flask_session import Session
 
 
 @app.route('/', methods=['GET', 'POST'])
